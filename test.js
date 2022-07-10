a9d6b44a6689f8c2ab325da5d387aaeb35e92b8a
Ehwuts Matsu <tsukan.git@gmail.com>
2022-07-09 20:49:38 -0700

    cli random build generation

--- test.js
+++ test.js
@@ -4,6 +4,27 @@ const slots = ["head", "chest", "legs", "feet", "offhand", "mainhand"];
 
 //console.log(JSON.stringify(data[0], null, "  "));
 
+let filter = {};
+
+filter.Valley = x => filterBy(x, "region", "valley");
+filter.Isles = x => filterBy(x, "region", "isles");
+
+filter.Helmets = x => filterBy(x, "slot", "head");
+filter.Chestplates = x => filterBy(x, "slot", "chest");
+filter.Leggings = x => filterBy(x, "slot", "legs");
+filter.Boots = x => filterBy(x, "slot", "feet");
+
+filter.Mainhands = x => filterBy(x, "slot", "mainhand");
+filter.Wands = x => filterBy(x, "enchantments", "Magic Wand");
+filter.Ranged = x => filterBy(x, "slot", "mainhand") && (filterBy(x, "attributes", "Throw Rate") || filterBy(x, "attributes", "Projectile Speed")) && !filterBy(x, "enchantments", "Riptide");
+filter.Melee = x => filterBy(x, "slot", "mainhand") && !((filterBy(x, "attributes", "Throw Rate") || filterBy(x, "attributes", "Projectile Speed")) && !filterBy(x, "enchantments", "Riptide"));
+filter.Mainhand_Swords = x => filterBy(x, "slot", "mainhand") && x.meta.type.toLowerCase().includes("sword");
+
+filter.Offhands = x => filterBy(x, "slot", "offhand");
+filter.Offhand_Swords = x => filterBy(x, "slot", "offhand") && x.meta.type.toLowerCase().includes("sword");
+filter.Offhand_Shields = x => filterBy(x, "slot", "offhand") && x.meta.type === "Shield";
+filter.Offhand_Not_Shield = x => filterBy(x, "slot", "offhand") && x.meta.type !== "Shield";
+
 const cache_names = {};
 
 function indexOfName(name) {
@@ -19,6 +40,10 @@ function indexOfName(name) {
 
 function getItem(name) {
 	return data[indexOfName(name)];
+}
+
+function randInt(a, b = 0) {
+	return Math.floor(Math.random() * Math.max(a, b)) + Math.min(a, b);
 }
 
 function getAttribute(item, attribute) {
@@ -53,8 +78,9 @@ function filterBy(item, category, content) {
 		case "region": return (item.Monumenta.Region === content);
 		case "slot": return (item.meta.slot === content);
 		case "type": return (item.meta.type === content);
-		case "name": return (item.plain.display.Name.toLowerCase().contains(content.toLowerCase()));
+		case "name": return (item.plain.display.Name.toLowerCase().includes(content.toLowerCase()));
 		case "enchantments": return item.Monumenta.Stock.Enchantments.hasOwnProperty(content);
+		case "location": return item.Monumenta.Location.toLowerCase() === content.toLowerCase();
 		case "attributes": 
 			let attributes = item.Monumenta.Stock.Attributes;
 			let L = attributes.length;
@@ -134,14 +160,40 @@ function listEnchantment(enchantment, mainhand = false, region = "all", filter =
 	listProperty("Enchantment", enchantment, mainhand, region, filter);
 }
 
+function getRandomBuild(region = randInt(2), mainhand = randInt(4)) {
+	let equipped = {};
+	
+	let regioned = region ? data.filter(filter.Isles) : data.filter(filter.Valley);	
+	//console.log(regioned);
+	
+	let helmets = regioned.filter(filter.Helmets);
+	let chestplates = regioned.filter(filter.Chestplates);
+	let leggings = regioned.filter(filter.Leggings);
+	let boots = regioned.filter(filter.Boots);
+
+	let mainhands = regioned.filter([filter.Wands, filter.Ranged, filter.Melee, filter.Mainhand_Swords][mainhand]);
+	let offhands = regioned.filter([filter.Offhands, filter.Offhand_Not_Shield, filter.Offhands, filter.Offhand_Swords][mainhand]);
+
+	equipped.Helmet = helmets[randInt(helmets.length)].plain.display.Name;
+	equipped.Chestplate = chestplates[randInt(chestplates.length)].plain.display.Name;
+	equipped.Leggings = leggings[randInt(leggings.length)].plain.display.Name;
+	equipped.Boots = boots[randInt(boots.length)].plain.display.Name;
+	equipped.Mainhand = mainhands[randInt(mainhands.length)].plain.display.Name;
+	equipped.Offhand = offhands[randInt(offhands.length)].plain.display.Name;
+	
+	console.log("Random Build |", region ? "Isles" : "Valley","Items |",["Wand", "Ranged", "Melee", "Dual Sword"][mainhand], "build");
+	console.log(equipped);
+}
+
 function printHelp() {
 	console.log("Usage: node", module.filename.split("\\").slice(-1)[0]);
 	console.log("\tname|tier|region|slot|type|enchantments|Attributes %");
 	console.log("\tmaximize|list Attribute|Enchantment % [mainhand=false] [region=all] [filter=null]");
+	console.log("\trandom [region=0-1] [weapon=0-3]");
 	//console.log(process.argv);	
 }
 
-if (process.argv.length >= 4) {
+if (process.argv.length >= 3) {
 	switch (process.argv[2].toLowerCase()) {
 		case "name":
 			console.log(JSON.stringify(getItem(process.argv[3]), null, "  ")); 
@@ -152,6 +204,7 @@ if (process.argv.length >= 4) {
 		case "type": 
 		case "enchantments": 
 		case "attributes": 
+		case "location":
 			let results = [];
 			
 			//console.log(process.argv[2], process.argv[3]);
@@ -161,6 +214,11 @@ if (process.argv.length >= 4) {
 				results.push(items[i].plain.display.Name);
 			}
 			console.log(results.length, results);
+			break;
+		case "random":
+			if (process.argv.length >= 5) getRandomBuild(process.argv[3]|0, process.argv[4]|0);
+			else if (process.argv.length >= 4) getRandomBuild(process.argv[3]|0);
+			else getRandomBuild();
 			break;
 		case "maximize":
 		{
@@ -179,3 +237,5 @@ if (process.argv.length >= 4) {
 	}
 	return;
 } else printHelp();
+
+getRandomBuild();
